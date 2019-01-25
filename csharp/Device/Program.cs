8652b599ad8ba250a4ac75ef6b3f7784d278950c
Janice D'Sa <jadsa@microsoft.com>
2019-01-25 09:43:42 -0800

    Add appsettings.json

--- csharp/Device/Program.cs
+++ csharp/Device/Program.cs
@@ -3,7 +3,10 @@
     using System;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Client;
+    using Microsoft.Extensions.Configuration.Json;
 
+    // Device side updator opens a device stream to IoTHub and waits for the service to connect to it
+    // After the service connects it opens a connection to the SSH daemon running on the device and tunnels traffic to it
     class Program
     {
         static Task Main(string[] args)
@@ -13,6 +16,14 @@
             Console.CancelKeyPress += delegate {
                 Console.WriteLine("Gateway updator exiting");
             };
+
+            // TODO: auto format extension
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true)
+            .Build();
+      Console.WriteLine($"Contents of Message Property: {message}");
+            // Open a connection to IoTHub streaming endpoint
+
             while(true);
         }
     }
